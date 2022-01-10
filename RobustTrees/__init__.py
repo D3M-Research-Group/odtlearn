d7fa60bc5d9bd0b76dcc4541deaddcf9a2359931
Patrick Vossler <patrick.vossler18@gmail.com>
2022-01-10 12:15:29 -0800

    add robust tree scaffolding

---
+++ RobustTrees/__init__.py
@@ -0,0 +1,8 @@
+from .RobustTree import RobustTreeEstimator
+from .RobustTree import RobustTreeClassifier
+from .RobustTree import RobustTreeTransformer
+
+from ._version import __version__
+
+__all__ = ['RobustTreeEstimator', 'RobustTreeClassifier', 'RobustTreeTransformer',
+           '__version__']
