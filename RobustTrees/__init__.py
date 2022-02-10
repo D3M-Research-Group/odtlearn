ba659aea585220518468183e42da1a840606ec80
Patrick Vossler <patrick.vossler18@gmail.com>
2022-02-09 20:54:00 -0800

    restructure code organization

--- RobustTrees/__init__.py
+++
@@ -1,8 +0,0 @@
-from .RobustTree import RobustTreeEstimator
-from .RobustTree import RobustTreeClassifier
-from .RobustTree import RobustTreeTransformer
-
-from ._version import __version__
-
-__all__ = ['RobustTreeEstimator', 'RobustTreeClassifier', 'RobustTreeTransformer',
-           '__version__']
--- StrongTrees/__init__.py
+++
@@ -1,8 +0,0 @@
-from .StrongTree import StrongTreeEstimator
-from .StrongTree import StrongTreeClassifier
-from .StrongTree import StrongTreeTransformer
-
-from ._version import __version__
-
-__all__ = ['StrongTreeEstimator', 'StrongTreeClassifier',
-           'StrongTreeTransformer', '__version__']
