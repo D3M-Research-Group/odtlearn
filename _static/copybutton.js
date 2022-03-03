07bb853b5fe0b8d7e97948844fd6ecd6f952c2e2
patrickvossler18 <patrickvossler18@users.noreply.github.com>
2022-03-03 23:47:32 +0000

    deploy: b0979c3bea8219269583fd24141830994218851d

--- _static/copybutton.js
+++ _static/copybutton.js
@@ -199,7 +199,7 @@ function formatCopyText(textContent, copybuttonPromptText, isRegexp = false, onl
 
 var copyTargetText = (trigger) => {
   var target = document.querySelector(trigger.attributes['data-clipboard-target'].value);
-  return formatCopyText(target.innerText, '', false, true, true, true, '', '')
+  return formatCopyText(target.innerText, '>>> ', false, true, true, true, '', '')
 }
 
   // Initialize with a callback so we can modify the text before copy
